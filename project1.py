6345f8b1ab416109724155e375099fdd6c9ad5a5
saniajain3 <114591774+saniajain3@users.noreply.github.com>
2024-04-03 18:18:41 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,50 @@
+import smtplib
+import pyaudio
+import speech_recognition  as sr
+from email.message import EmailMessage
+import pyttsx3
+#import distutils
+
+
+
+
+listener = sr.Recognizer()
+tts = pyttsx3.Engine()
+
+def talking_tom(text):
+    tts.say(text)
+    tts.runAndWait()
+
+
+def mic():
+    with sr.Microphone() as source:
+        print("Talk")
+        voice = listener.listen(source)
+        data = listener.recognize_google(voice)
+        print(data)
+        return data.lower()
+
+dict = {"test":"jainsania3@gmail.com"}
+
+def send_mail(receiver , subject , body):
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login("readright2022@gmail.com","dxghikiuidbtgzws")
+    email = EmailMessage()
+    email["from"] = "readright2022@gmail.com"
+    email["to"]= receiver
+    email["subject"] = subject
+    email.set_content(body)
+    server.send_message(email)
+
+def main_poc():
+    talking_tom("whom do you want to send this email ?")
+    name = mic()
+    receiver = dict[name]
+    talking_tom("what's the subject of the email")
+    subject = mic()
+    talking_tom("speak the body of the email")
+    body = mic()
+    send_mail(receiver , subject , body)
+    print("email sent successfully")
+main_poc()
